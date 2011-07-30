8500ac5e1881c35ba18c13fd044d1c7fa8f1c4a8
otstomcat <otstomcat@gmail.com>
2011-07-30 05:39:53 -0400

    added file to repo

---
+++ twitter_bot.py
@@ -0,0 +1,61 @@
+from twisted.words.protocols import irc
+from twisted.internet import reactor, protocol, task
+
+import time, sys, simplejson, urllib
+
+class TwitterBot(irc.IRCClient):
+	got_Pong = True
+	nickname = 'MLGSC2Scores'
+	last_tweet_id ='0'
+
+	def signedOn(self):
+		self.join(self.factory.channel)
+
+		self.lc = task.LoopingCall(self.update_twitter)
+		self.lc.start(24, False)
+		
+	def privmsg(self, user, channel, msg):
+		user = user.split('!', 1)[0]
+
+	def update_twitter(self):
+		rate_limit_status=get_rate_status()
+		
+		if(int(rate_limit_status['remaining_hits']) > 1):
+			last_tweet = get_last_tweet('mlgsc2scores')
+
+			if(long(self.last_tweet_id) < long(last_tweet['id'])):		
+				self.last_tweet_id = last_tweet['id']
+				self.say('testchannel', last_tweet['text'], length=None)
+		else:
+			self.msg('OTSTomcat','Rate limit reached!')
+
+class TwitterBotFactory(protocol.ClientFactory):
+	protocol = TwitterBot
+
+	def __init__(self, channel):
+		self.channel = channel
+	
+	def clientConnectionLost(self, connector, reason):
+		connector.connect()
+
+	def clientConnectionFailed(self, connector, reason):
+		print "connection failed: ", reason
+		reactor.stop()
+
+def get_last_tweet(user):
+	url = "https://twitter.com/statuses/user_timeline.json?id=" + user
+	result = simplejson.load(urllib.urlopen(url))
+	
+	return result[0]
+
+def get_rate_status():
+	url = "http://api.twitter.com/1/account/rate_limit_status.json"
+	result = simplejson.load(urllib.urlopen(url))
+
+	return result
+if __name__ == '__main__':
+
+	f = TwitterBotFactory('testchannel')
+	reactor.connectTCP("irc.quakenet.org", 6667, f)
+	reactor.run()
+		

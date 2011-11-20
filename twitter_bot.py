1945ac57005a9b1e6a0fae969c653afffef05c83
t0mcat <t0mcat@fencepost.local>
2011-11-19 20:45:48 -0500

    Generified bot to no longer be implementation-specific.

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,3 +1,4 @@
+import ConfigParser
 import sys
 from twisted.words.protocols import irc
 from twisted.internet import reactor, protocol, task
@@ -7,12 +8,18 @@ import simplejson, urllib
 
 class TwitterBot(irc.IRCClient):
     got_Pong = True
-    nickname = 'MLGSC2Scores'
-    password = 'kD3pbGTiPg'
     last_tweet_id ='0'
 
+    def __init__(self, config):
+        self.nickname = config.get('twitter_bot', 'nickname')
+        self.password = config.get('twitter_bot', 'password')
+        self.owner = config.get('twitter_bot', 'owner')
+        self.channels = config.get('twitter_bot', 'channels').split(', ')
+        self.trigger = config.get('twitter_bot', 'trigger')
+        self.twitter_account = config.get('twitter_bot', 'twitter_account')
+
     def signedOn(self):
-        for chan in self.factory.channel:
+        for chan in self.channels:
             log.msg('Joining channel ' + chan)
             self.join(chan)
 
@@ -23,24 +30,43 @@ class TwitterBot(irc.IRCClient):
         rate_limit_status=get_rate_status()
         
         if int(rate_limit_status['remaining_hits']) > 1:
-            last_tweet = get_last_tweet('mlgsc2scores')
+            last_tweet = get_last_tweet(self.twitter_account)
 
             if long(self.last_tweet_id) < long(last_tweet['id']):
                 log.msg('Latest tweet: ' + last_tweet['text'])
                     
                 self.last_tweet_id = last_tweet['id']
-                for channel in channels:
-                    self.say(channel, last_tweet['text'])
+                for chan in self.channels:
+                    self.say(chan, last_tweet['text'])
         else:
             log.msg('Rate limit reached!')
-            self.msg('OTSTomcat','Rate limit reached!')
+            self.msg(self.owner,'Rate limit reached!')
+
+    def privmsg(self, user, channel, msg):
+        user = user.split('!', 1)[0]
+
+        if channel == self.nickname:
+            log.msg('Message received: <' + user + '> ' + msg)
+
+            if user == self.owner and self.trigger in msg:
+                if 'addchannel' in msg:
+                    chan = msg.split()[1]
+                    self.join(chan)
+                    self.channels.append(chan)
+
+                if 'leavechannel' in msg:
+                    chan = msg.split()[1]
+                    self.leave(chan)
+                    self.channels.remove(chan)
 
 class TwitterBotFactory(protocol.ClientFactory):
-    protocol = TwitterBot
-
-    def __init__(self, channels):
-        self.channel = channels
     
+    def __init__(self, config):
+        self.config = config
+
+    def buildProtocol(self, addr):
+        return TwitterBot(self.config)
+
     def clientConnectionLost(self, connector, reason):
         connector.connect()
 
@@ -59,13 +85,16 @@ def get_rate_status():
     result = simplejson.load(urllib.urlopen(url))
 
     return result
+
 if __name__ == '__main__':
 
     log.startLogging(sys.stdout)
 
-    channels = ['day9tv', 'mlg', 'starcraft2.no']
-    f = TwitterBotFactory(channels)
+    config = ConfigParser.ConfigParser()
+    config.read('twitter_bot.cfg')
+
+    f = TwitterBotFactory(config)
 
     log.msg('Connecting...')
-    reactor.connectTCP("irc.quakenet.org", 6667, f)
+    reactor.connectTCP(config.get('twitter_bot', 'irc_server'), int(config.get('twitter_bot', 'irc_server_port')), f)
     reactor.run()

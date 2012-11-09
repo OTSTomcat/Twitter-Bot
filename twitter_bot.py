2dcab175d9193cf8eb7b777509442d61bc9b9dac
OTSTomcat <otstomcat@gmail.com>
2012-11-09 02:47:01 +0000

    Removed all twitter code, starting over.

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,46 +1,25 @@
 import ConfigParser
 import sys
 from twisted.words.protocols import irc
-from twisted.internet import reactor, protocol, task
+from twisted.internet import reactor, protocol, threads
 from twisted.python import log
-
-import simplejson, urllib
 
 class TwitterBot(irc.IRCClient):
     got_Pong = True
     last_tweet_id ='0'
 
     def __init__(self, config):
+	self.config = config
         self.nickname = config.get('twitter_bot', 'nickname')
         self.password = config.get('twitter_bot', 'password')
         self.owner = config.get('twitter_bot', 'owner')
         self.channels = config.get('twitter_bot', 'channels').split(', ')
         self.trigger = config.get('twitter_bot', 'trigger')
-        self.twitter_account = config.get('twitter_bot', 'twitter_account')
 
     def signedOn(self):
         for chan in self.channels:
             log.msg('Joining channel ' + chan)
             self.join(chan)
-
-        self.lc = task.LoopingCall(self.update_twitter)
-        self.lc.start(24, False)
-
-    def update_twitter(self):
-        rate_limit_status=get_rate_status()
-        
-        if int(rate_limit_status['remaining_hits']) > 1:
-            last_tweet = get_last_tweet(self.twitter_account)
-
-            if long(self.last_tweet_id) < long(last_tweet['id']):
-                log.msg('Latest tweet: ' + last_tweet['text'])
-                    
-                self.last_tweet_id = last_tweet['id']
-                for chan in self.channels:
-                    self.say(chan, last_tweet['text'])
-        else:
-            log.msg('Rate limit reached!')
-            self.msg(self.owner,'Rate limit reached!')
 
     def privmsg(self, user, channel, msg):
         user = user.split('!', 1)[0]
@@ -74,24 +53,6 @@ class TwitterBotFactory(protocol.ClientFactory):
         print "connection failed: ", reason
         reactor.stop()
 
-def get_last_tweet(user):
-    url = "https://twitter.com/statuses/user_timeline.json?id=" + user
-    try:
-        result = simplejson.load(urllib.urlopen(url))
-    except:
-        log.msg("Could not get last tweet.  Will retry.")    
-        return get_last_tweet(user)
-    return result[0]
-
-def get_rate_status():
-    url = "http://api.twitter.com/1/account/rate_limit_status.json"
-    try:
-        result = simplejson.load(urllib.urlopen(url))
-    except:
-        log.msg("Could not get rate limit.  Will retry.")
-        return get_rate_status()
-    return result
-
 if __name__ == '__main__':
 
     log.startLogging(sys.stdout)
@@ -104,3 +65,4 @@ if __name__ == '__main__':
     log.msg('Connecting...')
     reactor.connectTCP(config.get('twitter_bot', 'irc_server'), int(config.get('twitter_bot', 'irc_server_port')), f)
     reactor.run()
+
